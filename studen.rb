36ae162d6e000f9ba3bf771471ba78a162d8ef7d
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-11-18 10:41:32 +0430

    Make required changes

--- studen.rb
+++ studen.rb
@@ -5,7 +5,7 @@ class Student < Person
 
   def initialize(age, _classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classrniloom
+    @classroom = classroom
   end
 
   def play_hooky
