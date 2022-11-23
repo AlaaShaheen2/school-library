fb016e21d28bc67356d087abdcf8d791d2415370
Ibnballo1 <belloabdullateef035@gmail.com>
2022-11-23 20:31:37 +0100

    Add test for initializing the objecty

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,15 @@
 require_relative '../book'
+# Test for initialize method
+describe Book do
+  it "should create instance of the book class with 2 parameters" do
+    book = Book.new("Title", "Author")
+    expect(book).to be_an_instance_of Book
+  end
+end
+
+# Test for title
+describe '#title' do
+  it "should return the right title" do
+    book = Book.new("My Title", )
+  end
+end
